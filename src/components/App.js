b27e0b7624beb32997cbb48b29940c7013cca6d2
rikilamadrid <riki.lamadrid@gmail.com>
2018-03-06 23:55:32 -0600

    added a second user for matching data dynamics

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from '../favicon.ico';
+import logo1 from '../favicon.ico';
+import logo2 from '../misiks.png';
 import ListCounter from './ListCounter';
 import TimeCounter from './TimeCounter';
 import Playlists from './Playlists';
@@ -22,56 +23,106 @@ class App extends Component {
         const {
             appHeader,
             app,
-            userStyle,
+            titleStyle1,
+            titleStyle2,
             pickle,
             input,
-            icon
+            icon,
+            userStyle
         } = styles;
 
         const {
-            name,
-            playlists
-        } = this.state.serverData.user;
+            user1,
+            user2
+        } = this.state.serverData;
 
-        let filteredPlaylists = playlists
+        let filteredPlaylists1 = user1.playlists
             .filter(playlist =>
                 playlist.name.toLowerCase().includes(
                     this.state.filterInput.toLowerCase())
         );
 
-        let totalSongs = filteredPlaylists.reduce((songs, eachPlaylists) => {
+        let totalSongs1 = filteredPlaylists1.reduce((songs, eachPlaylists) => {
             return songs.concat(eachPlaylists.songs)
         }, []);
 
-        let totalTime = totalSongs.reduce((sum, totalSongs) => {
-            return Math.round(sum + totalSongs.duration/3600)
+        let totalTime1 = totalSongs1.reduce((sum, totalSongs1) => {
+            return Math.round(sum + totalSongs1.duration/3600)
         }, 0);
 
-        let onChange = text => {this.setState({filterInput: text})}
+        // --------------------data manipulation user2----------------------------------------
+
+        let filteredPlaylists2 = user2.playlists
+            .filter(playlist =>
+                playlist.name.toLowerCase().includes(
+                    this.state.filterInput.toLowerCase())
+        );
+
+        let totalSongs2 = filteredPlaylists2.reduce((songs, eachPlaylists) => {
+            return songs.concat(eachPlaylists.songs)
+        }, []);
+
+        let totalTime2 = totalSongs2.reduce((sum, totalSongs2) => {
+            return Math.round(sum + totalSongs2.duration/3600)
+        }, 0);
+
+        let matchTime = (totalTime1 === totalTime2 && totalTime1 > 0);
+
+        let title = () => {
+            if (matchTime) {
+                return <h1 style={titleStyle2}>Sounds like a match :) !</h1>;
+            } else {
+                return <h1 style={titleStyle1}>What kind of music do you like?</h1>
+            }
+        };
+
+        let logo = () => {
+            if (matchTime) {
+                return <img src={logo2} className="App-logo" />
+            } else {
+                return <img src={logo1} className="App-logo" />
+            }
+        }
+
+        let onChange = text => {this.setState({filterInput: text})};
 
         return (
             <div>
                 <div style={appHeader}>
-                  <img src={logo} className="App-logo" />
-                  <h1 style={userStyle}>Sounds Like {name}'s playlist:</h1>
+                  {logo()}
+                  {title()}
+                  <div>
+                      <i style ={icon} class="fas fa-search"><img/></i>
+                      <input
+                          placeholder={"search for the match:"}
+                          style={input}
+                          type="text"
+                          onKeyUp={event => onChange(event.target.value)}
+                      />
+                  </div>
                 </div>
-                <div style={{display: 'flex', justifyContent: 'center'}}>
-                    <ListCounter playlists={filteredPlaylists.length} />
-                    <div>
-                        <i style ={icon} class="fas fa-search"> <img/></i>
-                        <input
-                            placeholder={"sounds like a match:"}
-                            style={input}
-                            type="text"
-                            onKeyUp={event => onChange(event.target.value)}
-                        />
+                <div>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                        <ListCounter playlists={filteredPlaylists1.length} />
+                        <h2 style={userStyle}>{user1.name}'s music:</h2>
+                        <TimeCounter playlists={totalTime1}/>
                     </div>
-                    <TimeCounter playlists={totalTime}/>
-                </div>
-                <div style={{display: 'flex', justifyContent: 'center'}}>
-                    {filteredPlaylists.map(playlist =>
-                            <Playlists playlists={playlist} />
-                    )}
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                        {filteredPlaylists1.map(playlist =>
+                                <Playlists playlists={playlist} />
+                        )}
+                    </div>
+
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                        <ListCounter playlists={filteredPlaylists2.length} />
+                        <h2 style={userStyle}>{user2.name}'s music:</h2>
+                        <TimeCounter playlists={totalTime2}/>
+                    </div>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                        {filteredPlaylists2.map(playlist =>
+                                <Playlists playlists={playlist} />
+                        )}
+                    </div>
                 </div>
             </div>
         );
@@ -79,19 +130,19 @@ class App extends Component {
 };
 
 let fakeServerData = {
-    user: {
+    user1: {
         name: 'Ricardo',
         playlists: [
             {
-                name: 'Mis favoritas',
+                name: 'Latin',
                 songs: [
                     {name: 'Surfer Calavera', duration: 2631},
-                    {name: 'Muerte en hawaii', duration: 8253},
-                    {name: 'Música Ligera', duration: 2216}
+                    {name: 'Muerte en hawaii', duration: 2253},
+                    {name: 'Música Ligera', duration: 4316}
                 ]
             },
             {
-                name: 'All about rock',
+                name: 'Rock and Roll',
                 songs: [
                     {name: 'Sympathy for the devil', duration: 6666},
                     {name: 'Confortably numb', duration: 3152},
@@ -99,19 +150,11 @@ let fakeServerData = {
                 ]
             },
             {
-                name: 'Electronic love',
+                name: 'Classical',
                 songs: [
-                    {name: 'One more time', duration: 6428},
-                    {name: '9 am', duration: 9191},
-                    {name: 'Porcelain', duration:3333}
-                ]
-            },
-            {
-                name: 'Reggaeton',
-                songs: [
-                    {name: 'Dale', duration: 2554},
-                    {name: 'Gasolina', duration: 3671},
-                    {name: 'Para que retozen', duration: 5555}
+                    {name: 'Mozart', duration: 6492},
+                    {name: 'Beethoven', duration: 3554},
+                    {name: 'Chopin', duration: 3321}
                 ]
             },
             {
@@ -120,6 +163,43 @@ let fakeServerData = {
                     {name: 'Shape of Water', duration: 7452},
                     {name: 'Amelie', duration: 9777},
                     {name: 'Interstellar', duration: 8888}
+                ]
+            }
+        ]
+    },
+    user2: {
+        name: 'Marina',
+        playlists: [
+            {
+                name: 'Heavy Metal',
+                songs: [
+                    {name: 'Crazy Train', duration: 6767},
+                    {name: 'Highway ti Heaven', duration: 7676},
+                    {name: 'Chop Suey', duration: 8686}
+                ]
+            },
+            {
+                name: 'Latin',
+                songs: [
+                    {name: 'Surfer Calavera', duration: 2631},
+                    {name: 'Muerte en hawaii', duration: 2253},
+                    {name: 'Música Ligera', duration: 4316}
+                ]
+            },
+            {
+                name: 'Soundtracks',
+                songs: [
+                    {name: 'Shape of Water', duration: 7452},
+                    {name: 'Amelie', duration: 9777},
+                    {name: 'Interstellar', duration: 8888}
+                ]
+            },
+            {
+                name: 'Reggae',
+                songs: [
+                    {name: 'Three Little Birds', duration: 7887},
+                    {name: 'Pupilas Lejanas', duration: 6659},
+                    {name: 'Welcome to Jamrock', duration: 8117}
                 ]
             },
             {
@@ -138,13 +218,18 @@ const styles = {
     appHeader: {
       backgroundColor: '#34443f',
       height: 180,
-      padding: 20,
+      padding: 40,
       color: 'white',
       'text-align': 'center'
     },
-    userStyle: {
+    titleStyle1: {
         fontSize: '1.5em',
         color: '#47d23b',
+        fontSize: '10'
+    },
+    titleStyle2: {
+        fontSize: '1.5em',
+        color: '#ff006d',
         fontSize: '10'
     },
     pickle: {
@@ -160,10 +245,13 @@ const styles = {
         'font-family': 'Quicksand'
     },
     icon: {
-        'margin-top': '30px',
-        'padding-top': '10px',
         'padding-right': '15px'
 
+    },
+    userStyle: {
+        fontSize: '1.5em',
+        color: '#47d23b',
+        fontSize: '10'
     }
 };
 
